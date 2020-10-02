fb17f1113e9e8356b475d005239ed73949f24073
yosbel <yosbel.penate@gmail.com>
2020-10-03 01:27:47 +0300

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class MainViewModel
+{
+    public MainViewModel()
+    {
+        UrlByteCount = new NotifyTaskCompletion<int>(
+        MyStaticService.CountBytesInUrlAsync("http://www.example.com"));
+    }
+    public NotifyTaskCompletion<int> UrlByteCount { get; private set; }
+}
---
+++ MyStaticService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+public static class MyStaticService
+{
+    public static async Task<int> CountBytesInUrlAsync(string url)
+    {
+        // Artificial delay to show responsiveness.
+        await Task.Delay(TimeSpan.FromSeconds(3)).ConfigureAwait(false);
+        // Download the actual data and count it.
+        using (var client = new HttpClient())
+        {
+            var data = await client.GetByteArrayAsync(url).ConfigureAwait(false);
+            return data.Length;
+        }
+    }
+}
